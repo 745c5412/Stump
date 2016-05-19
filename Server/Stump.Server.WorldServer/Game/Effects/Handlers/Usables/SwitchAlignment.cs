639d6b5732054764f8769fbca80dd668cb903eb4
Orochi <orochi@arkalys.com>
2016-05-19 13:56:41 +0200

    Add SwitchAlignment Fix SmithMagicPotionItem

---
+++ Server/Stump.Server.WorldServer/Game/Effects/Handlers/Usables/SwitchAlignment.cs
@@ -0,0 +1,29 @@
+﻿using Stump.DofusProtocol.Enums;
+using Stump.Server.WorldServer.Game.Actors.RolePlay.Characters;
+using Stump.Server.WorldServer.Game.Effects.Instances;
+using Stump.Server.WorldServer.Game.Items.Player;
+
+namespace Stump.Server.WorldServer.Game.Effects.Handlers.Usables
+{
+    [EffectHandler(EffectsEnum.Effect_SwitchAlignment)]
+    public class SwitchAlignment : UsableEffectHandler
+    {
+        public SwitchAlignment(EffectBase effect, Character target, BasePlayerItem item)
+            : base(effect, target, item)
+        {
+        }
+
+        public override bool Apply()
+        {
+            var integerEffect = Effect.GenerateEffect(EffectGenerationContext.Item) as EffectInteger;
+
+            if (integerEffect == null)
+                return false;
+
+            UsedItems = 1;
+            Target.ChangeAlignementSide((AlignmentSideEnum)integerEffect.Value);
+
+            return true;
+        }
+    }
+}
