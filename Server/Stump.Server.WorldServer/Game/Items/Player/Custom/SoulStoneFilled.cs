e62fef2a7180c5bf25d5654f9925e977565a930f
timorem <timorem@hotmail.fr>
2016-10-17 13:42:29 +0200

    soul stone still bugged

--- Server/Stump.Server.WorldServer/Game/Items/Player/Custom/SoulStoneFilled.cs
+++ Server/Stump.Server.WorldServer/Game/Items/Player/Custom/SoulStoneFilled.cs
@@ -76,7 +76,7 @@ namespace Stump.Server.WorldServer.Game.Items.Player.Custom
                 false, // todo : over max
                 Effects.Where(entry => !entry.Hidden).Select(entry => entry.GetObjectEffect()),
                 Guid,
-                (int)(Stack-1));
+                (int)(Stack));
         }
     }
 }
