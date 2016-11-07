5722711cd743e1f1555e015c1ae7f6d36dbdfb37
timorem <timorem@hotmail.fr>
2016-11-07 16:02:06 +0100

    fix mounts

--- Server/Stump.Server.WorldServer/Commands/Commands/MountCommand.cs
+++ Server/Stump.Server.WorldServer/Commands/Commands/MountCommand.cs
@@ -1,9 +1,12 @@
-﻿using Stump.Core.Extensions;
+﻿using NLog;
+using Stump.Core.Extensions;
 using Stump.DofusProtocol.Enums;
 using Stump.Server.BaseServer.Commands;
 using Stump.Server.WorldServer.Commands.Commands.Patterns;
 using Stump.Server.WorldServer.Commands.Trigger;
 using Stump.Server.WorldServer.Game.Actors.RolePlay.Mounts;
+using Stump.Server.WorldServer.Game.Exchanges.Paddock;
+using Stump.Server.WorldServer.Game.Maps.Paddocks;
 
 namespace Stump.Server.WorldServer.Commands.Commands
 {
@@ -33,6 +36,30 @@ namespace Stump.Server.WorldServer.Commands.Commands
             var mount = MountManager.Instance.CreateMount(trigger.Character, template);
 
             trigger.Character.EquipMount(mount);
+        }
+    }
+
+    public class MountPaddockCommand : InGameSubCommand
+    {
+        public MountPaddockCommand()
+        {
+            Aliases = new[] {"paddock"};
+            RequiredRole = RoleEnum.Administrator;
+            Description = "Open curent map paddock";
+            ParentCommandType = typeof(MountCommands);
+        }
+
+        public override void Execute(GameTrigger trigger)
+        {
+            var paddock = PaddockManager.Instance.GetPaddockByMap(trigger.Character.Map.Id);
+            if (paddock == null)
+            {
+                trigger.ReplyError("Paddock not found");
+                return;
+            }
+
+            var exchange = new PaddockExchange(trigger.Character, paddock);
+            exchange.Open();
         }
     }
 }
